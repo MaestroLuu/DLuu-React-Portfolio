dcc63740dc2b7783d6bf81fbe2bab86146ab1f6c
David Luu <dluu.audeo@gmail.com>
2022-03-03 09:46:49 -0800

    Updated Portfolio Container with switch case

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+import Home from "./pages/Home";
+import About from ".pages/About";
+import Contact from ".pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./prages/Resume";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+  // selects which page to render
+  const renderPage = () => {
+    switch (currentPage) {
+      case "Home":
+        return <Home />;
+      case "About":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Resume":
+        return <Resume />;
+      default:
+        return <Contact />;
+    }
+  };
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
